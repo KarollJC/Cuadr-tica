11eb19fce2538e4eec70c52ed5544e8e573d6058
KarollJC <juck080419hgtrvra0@cbtis217.edu.mx>
2024-10-16 19:18:03 -0600

    Único comentario explicando las funciones

--- Cuadratica.cpp
+++ Cuadratica.cpp
@@ -1,10 +1,14 @@
+//Este programa lo que hace es calcular las raices de x1 y x2
+//por medio de la formula general, lo primero que hace es declarar
+//las variables que se van a utilizar, despues se leen los valores
+// de a, b y c, despues se hace la formula general y se imprimen
+// los resultados de x1 y x2.
+
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-
-  //Se declaran las variables
+  
   double a, b, c, Op1=0, X1=0, X2=0;
-  //Se ingresaran los valores de a, b y c
   cout<<"Calcular raices con formula general"<<endl;
   cout<<"Valor de A: "<<endl;
   cin>>a;
@@ -12,20 +16,16 @@ int main() {
   cin>>b;
   cout<<"Valor de C: "<<endl;
   cin>>c;
-  //Si algun valor es 0 no se podra calcular y mostrara un mensaje
   if(a==0 || b==0 || c==0)
   {
     cout<<"No se puede calcular, los valores no pueden ser 0"<<endl;
   }
 
-  //Si ningun valor es 0 se podra calcular
   else {
-  //Formula general
   Op1=sqrt(((b*b)-(4*a*c)));
   X1=(-b+Op1)/(2*a);
   X2=(-b-Op1)/(2*a);
 
-  //Se imprimiran los valores de X1 y X2 a 4 decimales
   printf( "El valor de X1 es = %.4f\n", X1);
   printf( "El valor de X2 es = %.4f\n", X2);
   }
