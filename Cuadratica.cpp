b936ca2777101547d5436f622784fda1731856c4
KarollJC <juck080419hgtrvra0@cbtis217.edu.mx>
2024-10-16 18:57:03 -0600

    Se agregaron comentarios al codigo

--- Cuadratica.cpp
+++ Cuadratica.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 int main() {
 
+  //Se declaran las variables
   double a, b, c, Op1=0, X1=0, X2=0;
+  //Se ingresaran los valores de a, b y c
   cout<<"Calcular raices con formula general"<<endl;
   cout<<"Valor de A: "<<endl;
   cin>>a;
@@ -10,16 +12,20 @@ int main() {
   cin>>b;
   cout<<"Valor de C: "<<endl;
   cin>>c;
+  //Si algun valor es 0 no se podra calcular y mostrara un mensaje
   if(a==0 || b==0 || c==0)
   {
     cout<<"No se puede calcular, los valores no pueden ser 0"<<endl;
   }
 
+  //Si ningun valor es 0 se podra calcular
   else {
+  //Formula general
   Op1=sqrt(((b*b)-(4*a*c)));
   X1=(-b+Op1)/(2*a);
   X2=(-b-Op1)/(2*a);
 
+  //Se imprimiran los valores de X1 y X2 a 4 decimales
   printf( "El valor de X1 es = %.4f\n", X1);
   printf( "El valor de X2 es = %.4f\n", X2);
   }
